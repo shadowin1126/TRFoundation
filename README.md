65f230b0ee204faedaae7c9348cf06053c11131a
“shadowin1126git config --global user.name “shadowin1126 <shadowin1126@yahoo.com>
2015-06-05 04:46:15 +0800

    Top Radio Foundation theme

--- README.md
+++ README.md
@@ -177,3 +177,4 @@ Pull requests are highly appreciated. More than three dozen amazing people have 
 
 * [Zurb Foundation Docs](http://foundation.zurb.com/docs/)
 * [WordPress Codex](http://codex.wordpress.org/)
+# topradio
--- parts/top-bar.php
+++ parts/top-bar.php
@@ -8,16 +8,26 @@
  */
 
 ?>
-<div class="top-bar-container contain-to-grid show-for-medium-up">
+<div class="top-bar-container contain-to-grid sticky show-for-medium-up">
     <nav class="top-bar" data-topbar role="navigation">
-        <ul class="title-area">
-            <li class="name">
-                <h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-            </li>
-        </ul>
-        <section class="top-bar-section">
-            <?php foundationpress_top_bar_l(); ?>
-            <?php foundationpress_top_bar_r(); ?>
-        </section>
+        <div class="medium-2 columns">
+        	<ul class="title-area">
+            	<li class="name">
+            	    <b><a href="http://top-radio.org"><img src="http://top-radio.org/wp-content/uploads/2015/05/TR01.png"></a></b>
+          	 	</li>
+        	</ul>
+        </div>
+			<section class="top-bar-section">
+				<div class="large-8 columns">
+					<ul class="left">
+						<li><a href="/malaysia/">Malaysia</a></li>
+						<li><a href="/thailand/">Thailand</a></li>
+					</ul>
+				</div>
+				<div class="large-2 columns">
+					<a href="https://www.facebook.com/OnlineTopRadio"><img title="Facebook" alt="Facebook" src="http://top-radio.org/wp-content/uploads/2015/05/fb-break.png" width="35" height="35" /></a>
+					<a href="https://twitter.com/OnlineTopRadio"><img title="Twitter" alt="Twitter" src="http://top-radio.org/wp-content/uploads/2015/05/tw-break.png" width="35" height="35" /></a>
+				</div>
+			</section>
     </nav>
 </div>
